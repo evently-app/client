473db457f189529c34754fcfc2765104b742dae1
Alexi Christakis <alexi.christakis@gmail.com>
2019-02-24 17:40:56 -0500

    setting up navigation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import App from "./components/App";
+// import { Navigation } from "react-native-navigation";
 
-// handle code push, redux store initialization, navigation, authenticate w/ firebase
+// // import App from "./components/App";
+
+// // handle code push, redux store initialization, navigation, authenticate w/ firebase
+
+// Navigation.events().registerAppLaunchedListener(() => {
+// 	Navigation.setRoot({
+// 		root: {
+// 			component: {
+// 				name: "evently.App"
+// 			}
+// 		}
+// 	});
+// });
---
+++ src/screens.js
@@ -0,0 +1,8 @@
+import { Navigation } from "react-native-navigation";
+
+import App from "./components/App";
+
+export default (Provider, store) => {
+	/* register components */
+	Navigation.registerComponent("evently.App", () => App, Provider, store);
+};
