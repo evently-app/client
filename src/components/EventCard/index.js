5ae9bdd8e05ca5963e3a3fa0592ab3995de059e6
Alexi Christakis <alexi.christakis@gmail.com>
2019-03-01 19:22:58 -0500

    scroll to event card

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -1,22 +1,32 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { ScrollView, View, Text } from "react-native";
 
 import { SCREEN_WIDTH, SB_HEIGHT, SCREEN_HEIGHT } from "../../lib/constants";
 
 class EventCard extends Component {
 	render() {
 		return (
-			<View
+			<ScrollView
 				style={{
 					width: SCREEN_WIDTH - 20,
 					height: SCREEN_HEIGHT - 200,
+					// alignItems: "center",
+					// justifyContent: "center",
 					// marginBottom: 100,
-					backgroundColor: this.props.backgroundColor,
+					backgroundColor: "white",
 					borderRadius: 20
 				}}
 			>
+				<View
+					style={{
+						backgroundColor: this.props.backgroundColor,
+						width: SCREEN_WIDTH - 20,
+						height: SCREEN_HEIGHT - 200
+					}}
+				/>
 				<Text>{this.props.title}</Text>
-			</View>
+				<View style={{ height: SCREEN_HEIGHT }} />
+			</ScrollView>
 		);
 	}
 }
