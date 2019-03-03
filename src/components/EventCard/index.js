ec9e3add101fb3a63d901531619dc1650debcc28
Alexi Christakis <alexi.christakis@gmail.com>
2019-03-02 19:40:28 -0500

    scroll indicator

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Animated, View, Text } from "react-native";
+import { Animated, View, Text, StyleSheet } from "react-native";
 
 import ActionButton from "./ActionButton";
 
@@ -13,38 +13,72 @@ class EventCard extends Component {
 	});
 
 	render() {
+		const animatedScrollIndicator = {
+			transform: [
+				{
+					translateY: this.yOffset.interpolate({
+						inputRange: [0, SCREEN_HEIGHT],
+						outputRange: [0, 100]
+					})
+				}
+			]
+		};
+
 		return (
-			<View
-				style={{
-					overflow: "hidden",
-					width: SCREEN_WIDTH - 20,
-					height: SCREEN_HEIGHT - 200,
-					// alignItems: "center",
-					// justifyContent: "center",
-					// marginBottom: 100,
-					backgroundColor: "white",
-					borderRadius: 20
-				}}
-			>
+			<View style={styles.container}>
 				<Animated.ScrollView
 					showsVerticalScrollIndicator={false}
 					scrollEventThrottle={16}
 					onScroll={this._onScroll}
 				>
 					<View
-						style={{
-							backgroundColor: this.props.backgroundColor,
-							width: SCREEN_WIDTH - 20,
-							height: SCREEN_HEIGHT - 200
-						}}
+						style={[styles.coloredBackground, { backgroundColor: this.props.backgroundColor }]}
 					/>
 					<Text>{this.props.title}</Text>
 					<View style={{ height: SCREEN_HEIGHT }} />
 				</Animated.ScrollView>
 				<ActionButton yOffset={this.yOffset} />
+				<View style={styles.scrollContainer}>
+					<Animated.View style={[styles.scrollIndicator, animatedScrollIndicator]} />
+				</View>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		overflow: "hidden",
+		width: SCREEN_WIDTH - 20,
+		height: SCREEN_HEIGHT - 200,
+		// alignItems: "center",
+		// justifyContent: "center",
+		// marginBottom: 100,
+		backgroundColor: "white",
+		borderRadius: 20
+	},
+	coloredBackground: {
+		width: SCREEN_WIDTH - 20,
+		height: SCREEN_HEIGHT - 200
+	},
+	scrollContainer: {
+		position: "absolute",
+		right: 10,
+		top: 10,
+		width: 7,
+		height: 120,
+		backgroundColor: "gray",
+		borderRadius: 10
+	},
+	scrollIndicator: {
+		position: "absolute",
+		right: 0,
+		top: 0,
+		width: 7,
+		height: 20,
+		backgroundColor: "white",
+		borderRadius: 10
+	}
+});
 
 export default EventCard;
