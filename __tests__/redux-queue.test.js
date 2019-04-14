1e7188e0c7c15fd806a713b87998abcb265117dd
leaspark <lea.sparkman@yale.edu>
2019-04-14 16:50:53 -0400

    more redux tests

---
+++ __tests__/redux-queue.test.js
@@ -0,0 +1,57 @@
+import {
+	RESET_QUEUE,
+	LOAD_QUEUE_INIT,
+	LOAD_QUEUE_SUCCESS,
+	LOAD_QUEUE_FAILURE,
+	POP,
+	loadQueueInit,
+	loadQueueSuccess,
+	loadQueueFailure,
+	resetQueue,
+	pop
+} from "../src/redux/queue";
+
+describe("actions", () => {
+	it("Test reset queue", () => {
+		const expectedAction = {
+			type: RESET_QUEUE
+		};
+		expect(resetQueue()).toEqual(expectedAction);
+	});
+	it("Test load queue init ", () => {
+		const expectedAction = {
+			type: LOAD_QUEUE_INIT
+		};
+		expect(loadQueueInit()).toEqual(expectedAction);
+	});
+	it("Test loadQueue_success ", () => {
+		const events = [];
+		const lastDoc = [];
+		const filterType = "";
+		const filterTime = "";
+		const expectedAction = {
+			type: LOAD_QUEUE_SUCCESS
+			// events,
+			// lastDoc,
+			//filterTime,
+			//filterType
+		};
+		expect(
+			loadQueueSuccess(events, lastDoc, filterType, filterType)
+		).toEqual(expectedAction);
+	});
+	it("Test load queue failure ", () => {
+		const error = "error message";
+		const expectedAction = {
+			type: LOAD_QUEUE_FAILURE
+			//error
+		};
+		expect(loadQueueFailure()).toEqual(expectedAction);
+	});
+	it("Test pop ", () => {
+		const expectedAction = {
+			type: POP
+		};
+		expect(pop()).toEqual(expectedAction);
+	});
+});
